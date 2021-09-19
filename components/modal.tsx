c6aa057540496d1a65c1552d985d2d529d246535
chocopc123 <0215ryuuku@gmail.com>
2021-09-19 16:27:26 +0900

    モーダル背景を押下で閉じるように修正

--- components/modal.tsx
+++ components/modal.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import styles from '../styles/Modal.module.css';
+import Image from 'next/image';
 
 type Props = {
   name: string;
@@ -16,15 +17,20 @@ export default class Modal extends React.Component<Props> {
     return (
       <div className="modal-wrap">
         <div className={styles.modalArea}>
-          <div className={styles.modalBackground} />
+          <div className={styles.modalBackground} onClick={this.props.toggleModal} />
           <div className={styles.modalWrapper}>
             <div className="modalContents">
-              <h3 className="text-3xl font-bold">Gif</h3>
+              <h3 className="text-3xl font-bold">{this.props.data.title}</h3>
               <hr className="my-2" />
-              <p>楽しいgif</p>
+              <Image
+                src={this.props.data.images.fixed_width.url}
+                alt={this.props.data.title}
+                width={this.props.data.images.fixed_width.width}
+                height={this.props.data.images.fixed_width.height}
+              />
               <hr className="my-2" />
               <button className={styles.button} onClick={this.props.toggleModal}>
-                Close modal
+                Close
               </button>
             </div>
           </div>
