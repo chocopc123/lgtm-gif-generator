fc93f7e0259cf5ffd0e8a0158bd797351a6d65f3
chocopc123 <0215ryuuku@gmail.com>
2021-09-23 22:00:23 +0900

    検索フォームの値をurlに反映

--- pages/generator.tsx
+++ pages/generator.tsx
@@ -12,8 +12,9 @@ export default function Generator({ gifs }: { gifs: any }) {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <div className="w-3/5">
-        <form className="flex justify-center mt-10">
+        <div className="flex justify-center mt-10">
           <input
+            id="search"
             type="search"
             name="search"
             className="inline-block bg-white text-gray-700 border border-gray-300 rounded py-2 px-4 mr-1 focus:outline-none focus:bg-white focus:border-gray-400 w-3/6"
@@ -22,8 +23,9 @@ export default function Generator({ gifs }: { gifs: any }) {
             type="submit"
             value="Search"
             className="inline-block bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-2 rounded w-1/6 content-center"
+            onClick={() => getGif()}
           />
-        </form>
+        </div>
       </div>
       <div className={styles.grid}>
         {gifs.data.map((data: any) => (
@@ -32,6 +34,18 @@ export default function Generator({ gifs }: { gifs: any }) {
       </div>
     </div>
   );
+}
+
+function getGif() {
+  const searchInput = document.getElementById('search') as HTMLInputElement;
+  const searchString = searchInput.value;
+  changeUrlParams(searchString);
+}
+
+function changeUrlParams(searchString: string) {
+  const url = new URL(window.location.href);
+  url.searchParams.set('search', searchString);
+  window.history.replaceState({}, '', `${url.toString()}`);
 }
 
 export const getStaticProps: GetStaticProps = async () => {
