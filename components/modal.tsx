a0e6aa43350a3921f8542a99f874740d897a8d82
chocopc123 <0215ryuuku@gmail.com>
2021-10-27 00:02:36 +0900

    modalコンポーネントの変更

--- components/modal.tsx
+++ components/modal.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from '../styles/Modal.module.css';
 import Image from 'next/image';
 import Loading from '../components/loading';
@@ -13,83 +13,72 @@ type Props = {
   showModal: boolean;
 };
 
-type State = {
-  showModal: boolean;
-};
+const Modal = (props: Props) => {
+  const [showModal, setShowModal] = useState(props.showModal);
+  return (
+    <div className={styles.modalArea}>
+      <CSSTransition
+        in={showModal}
+        appear={true}
+        timeout={200}
+        classNames={{
+          appear: styles.modalBackgroundEnter,
+          appearActive: styles.modalBackgroundEnterActive,
+          exit: styles.modalBackgroundExit,
+          exitActive: styles.modalBackgroundExitActive,
+        }}
+        unmountOnExit
+        onExited={props.toggleModal}
+      >
+        <div className={styles.modalBackground} onClick={() => closeModal()} />
+      </CSSTransition>
+      <CSSTransition
+        in={showModal}
+        appear={true}
+        timeout={200}
+        classNames={{
+          appear: styles.modalEnter,
+          appearActive: styles.modalEnterActive,
 
-export default class Modal extends React.Component<Props, State> {
-  constructor(props: any) {
-    super(props);
-    this.state = {
-      showModal: this.props.showModal,
-    };
-  }
-  render() {
-    return (
-      <div className={styles.modalArea}>
-        <CSSTransition
-          in={this.state.showModal}
-          appear={true}
-          timeout={200}
-          classNames={{
-            appear: styles.modalBackgroundEnter,
-            appearActive: styles.modalBackgroundEnterActive,
-            exit: styles.modalBackgroundExit,
-            exitActive: styles.modalBackgroundExitActive,
-          }}
-          unmountOnExit
-          onExited={this.props.toggleModal}
-        >
-          <div className={styles.modalBackground} onClick={() => this.closeModal()} />
-        </CSSTransition>
-        <CSSTransition
-          in={this.state.showModal}
-          appear={true}
-          timeout={200}
-          classNames={{
-            appear: styles.modalEnter,
-            appearActive: styles.modalEnterActive,
-
-            exit: styles.modalExit,
-            exitActive: styles.modalExitActive,
-          }}
-          unmountOnExit
-        >
-          <div className={styles.modalWrapper}>
-            <div className="modalContents">
-              <h3 className="text-3xl font-bold">{this.props.data.title}</h3>
-              <hr className="my-2" />
-              <Image
-                src={this.props.data.images.fixed_width.url}
-                alt={this.props.data.title}
-                width={this.props.data.images.fixed_width.width}
-                height={this.props.data.images.fixed_width.height}
-              />
-              {!this.props.generateGifLoaded && <Loading />}
-              <img
-                id="preview"
-                alt={'[Preview] ' + this.props.data.title}
-                className={(this.props.generateGifLoaded ? 'visible' : 'invisible') + ' border'}
-              />
-              <hr className="my-2" />
-              <button className={styles.closeButton} onClick={this.props.toggleModal}>
-                Close
-              </button>
-              <button
-                className={styles.downloadButton}
-                onClick={this.downloadGif}
-                disabled={!this.props.generateGifRendered}
-              >
-                Download
-              </button>
-            </div>
+          exit: styles.modalExit,
+          exitActive: styles.modalExitActive,
+        }}
+        unmountOnExit
+      >
+        <div className={styles.modalWrapper}>
+          <div className="modalContents">
+            <h3 className="text-3xl font-bold">{props.data.title}</h3>
+            <hr className="my-2" />
+            <Image
+              src={props.data.images.fixed_width.url}
+              alt={props.data.title}
+              width={props.data.images.fixed_width.width}
+              height={props.data.images.fixed_width.height}
+            />
+            {!props.generateGifLoaded && <Loading />}
+            <img
+              id="preview"
+              alt={'[Preview] ' + props.data.title}
+              className={(props.generateGifLoaded ? 'visible' : 'invisible') + ' border'}
+            />
+            <hr className="my-2" />
+            <button className={styles.closeButton} onClick={props.toggleModal}>
+              Close
+            </button>
+            <button
+              className={styles.downloadButton}
+              onClick={downloadGif}
+              disabled={!props.generateGifRendered}
+            >
+              Download
+            </button>
           </div>
-        </CSSTransition>
-      </div>
-    );
-  }
+        </div>
+      </CSSTransition>
+    </div>
+  );
 
-  downloadGif() {
+  function downloadGif() {
     const preview = document.getElementById('preview') as HTMLImageElement;
     const a = document.createElement('a');
     document.body.appendChild(a);
@@ -99,7 +88,9 @@ export default class Modal extends React.Component<Props, State> {
     a.remove();
   }
 
-  closeModal() {
-    this.setState({ showModal: false });
+  function closeModal() {
+    setShowModal(false);
   }
-}
+};
+
+export default Modal;
